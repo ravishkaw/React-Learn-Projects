ea6a36bc409b57ac29f141f5a3528534f059363b
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-03-21 20:08:36 +0530

    added book component and created a working project

---
+++ bookapp/src/components/App.js
@@ -0,0 +1,14 @@
+import Books from "./Books";
+import "./App.css"
+
+function App() {
+  return (
+    <section className="booklist">
+      <Books />
+      <Books />
+      <Books />
+    </section>
+  );
+}
+
+export default App;
--- bookapp/src/components/Books.js
+++ bookapp/src/components/Books.js
@@ -1,3 +1,22 @@
-const Books = () => {};
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
+    alt="Interesting Facts For Curious Minds"
+  />
+);
+const Title = () => {
+  return <h2>Interesting Facts For Curious Minds</h2>;
+};
+const Author = () => <h4>Jordan Moore </h4>;
+
+const Books = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
 export default Books;
