10e2a027181eef3a0872d8d3b7208e78d0bfd0ae
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-03-23 20:07:31 +0530

    add some book list via array of objects

---
+++ bookapp/src/components/book.js
@@ -0,0 +1,33 @@
+import img1 from "../assests/images/img1.jpg"
+import img2 from "../assests/images/img2.jpg"
+import img3 from "../assests/images/img3.jpg"
+import img4 from "../assests/images/img4.jpg"
+
+const books = [
+  {
+    id: 1,
+    author: "Dav Pilkey",
+    title: "Dog Man: The Scarlet Shedder",
+    img: img1,
+  },
+  {
+    id: 2,
+    author: "Sharon McMahon",
+    title: "The Small and the Mighty",
+    img: img2,
+  },
+  {
+    id: 3,
+    author: "Kristin Hannah",
+    title: "The Women",
+    img: img3,
+  },
+  {
+    id: 4,
+    author: "Marilyn Sadler",
+    title: "It's Not Easy Being a Bunny",
+    img: img4,
+  },
+];
+
+export default books;
