8c89cb80b6721aca3987c2bd1007178550de9f9b
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-03-25 18:35:16 +0530

    add and update data file

---
+++ luckyshrub/src/components/datalist.js
@@ -0,0 +1,96 @@
+export const pageLinks = [
+  { id: 1, href: "#", text: "Home Page" },
+  { id: 2, href: "#services", text: "Services" },
+  { id: 3, href: "#about_us", text: "About Us" },
+  { id: 4, href: "#contact_us", text: "Contact Us" },
+];
+
+export const topCards = [
+  {
+    id: 1,
+    title: "Our Services",
+    content:
+      `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et <a href='#services'> See our services </a>`,
+  },
+  {
+    id: 2,
+    title: "What's New",
+    content:
+      " Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et",
+  },
+  {
+    id: 3,
+    title: "Opening Hours",
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br /><br />Mon - Fri : 8 am - 10 pm<br />Sat - Sun : 9 am - 11 pm",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    title: "Garden Design and Creation",
+    content:
+      "Our team of skilled professionals, led by Jason, our talented garden architect, specializes in creating stunning and personalized garden designs. Whether you're looking for a tranquil retreat or a vibrant outdoor space, we work closely with our clients to bring their vision to life. From conceptualization to installation, we take care of every detail to ensure a seamless and hassle-free experience.",
+  },
+  {
+    id: 2,
+    title: "Maintenance and Landscaping",
+    content:
+      "At Lucky Shrub, we understand that maintaining a beautiful garden requires time and effort. That's why our dedicated team of landscapers is here to help. We offer regular maintenance services to keep your garden in pristine condition throughout the year. From lawn mowing and pruning to fertilizing and pest control, we've got you covered. Our experts have the knowledge and skills to ensure your garden remains healthy and vibrant.",
+  },
+  {
+    id: 3,
+    title: "Plant Nursery",
+    content:
+      "As a one-stop shop for all your gardening needs, we also operate a small plant nursery. Our nursery offers a wide variety of indoor and outdoor plants, carefully selected for their beauty and durability. Whether you're a seasoned gardener or just starting out, our knowledgeable staff is here to assist you in choosing the perfect plants for your space. From colorful flowers to lush foliage, we have everything you need to create your dream garden.",
+  },
+];
+
+export const reasons = [
+  {
+    id: 1,
+    title: "Expertise:",
+    content:
+      "With years of experience in the industry, our team brings a wealth of knowledge and expertise to every project.",
+  },
+  {
+    id: 2,
+    title: "Personalized Approach:",
+    content:
+      " We believe in creating unique and customized designs that reflect the individual style and preferences of our clients.",
+  },
+  {
+    id: 3,
+    title: "Attention to Detail:",
+    content:
+      "From the initial consultation to the final installation, we pay close attention to every detail to ensure the highest quality results",
+  },
+  {
+    id: 4,
+    title: "Customer Satisfaction:",
+    content:
+      "We prioritize customer satisfaction and strive to exceed expectations with our exceptional service and professionalism.",
+  },
+  {
+    id: 5,
+    title: "One-Stop Shop:",
+    content:
+      "With our range of services and plant nursery, we provide a convenient and comprehensive solution for all your gardening needs.",
+  },
+];
+
+export const teams = [
+  {
+    id: 1,
+    name: "Jason - Garden Architect : ",
+    details:
+      "Jason, our talented garden architect, is the creative force behind every design at Lucky Shrub. With years of experience and a keen eye for detail, he meticulously crafts captivating outdoor spaces that reflect your style and preferences. Jason also manages our team of skilled landscapers, ensuring that every project is executed with precision and expertise.",
+  },
+  {
+    id: 2,
+    name: "Maria - Marketing Manager : ",
+    details:
+      "Maria is the marketing mastermind behind Lucky Shrub. With her strategic mindset and creative flair, she ensures that our brand reaches the right audience and communicates our values effectively. Maria also oversees our plant nursery, where she curates a collection of top-quality indoor and outdoor plants for our clients to enjoy.",
+  },
+];
