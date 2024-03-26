f7b9023eebbfe6f1b0e39d758a13562cdf8e67c0
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-03-26 19:22:42 +0530

    update all components and finalize

--- birthdayreminder/src/App.jsx
+++ birthdayreminder/src/App.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./components/List";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  const clearList = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people} />
+        <button className="btn btn-block" onClick={clearList}>
+          Clear List
+        </button>
+      </section>
+    </main>
+  );
 };
 export default App;
